650b835015436913f2c19355fdbb1cbcbcd323a5
tdshe <tnshvts1602@gmail.com>
2024-11-29 14:51:19 +0200

    HW7

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import dto.UserDto;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -18,8 +19,35 @@ public class LoginPage extends BasePage
     @FindBy(xpath = "//input[@name='email']")
     WebElement inputEmail;
 
-    public void typeLoginForm(String email, String password)
+    @FindBy(xpath = "//input[@name='password']")
+    WebElement inputPwd;
+
+    @FindBy(xpath = "//button[text()='Login']")
+    WebElement btnLogin;
+
+    @FindBy(xpath = "//button[text()='Registration']")
+    WebElement btnRegister;
+
+    public void typeLoginForm(UserDto user)
+    {
+        inputEmail.sendKeys(user.getEmail());
+        inputPwd.sendKeys(user.getPassword());
+
+        pause(2);
+
+        btnLogin.click();
+        pause(2);
+    }
+
+    public void typeRegistrationForm(String email, String password)
     {
         inputEmail.sendKeys(email);
+        inputPwd.sendKeys(password);
+
+        pause(2);
+
+        btnRegister.click();
+
+        pause(2);
     }
 }
--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -5,12 +5,15 @@ import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
 
+import java.util.Random;
+
 public class RegistrationTests extends ApplicationManager
 {
     @Test
-    public void registrationPositiveTest()
+    public void RegistrationPositiveTest()
     {
+        int i = new Random().nextInt(1000);
         new HomePage(getDriver()).clickBtnLoginHeader();
-        new LoginPage(getDriver()).typeLoginForm("my_email", "pwd");
+        new LoginPage(getDriver()).typeRegistrationForm("frosiaH_"+i+"@gmail.com", "Trjjs199!!");
     }
 }
